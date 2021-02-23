53b5e2bc25566538b0b8208a84251c0780d52ad5
ssanchezg52 <72250716+ssanchezg52@users.noreply.github.com>
2021-02-23 12:21:46 +0100

    Update EjercicioTrece.java

--- src/ejerciciosuno/EjercicioTrece.java
+++ src/ejerciciosuno/EjercicioTrece.java
@@ -5,7 +5,7 @@ public class EjercicioTrece {
 		String cadena = "la.cosa.es.que.vamos.a.hacer.muchos.trocitos";
 		char punto = '.';
 		//Tan solo hay que usar el parametro limit. Estudia el resultado
-		String resultado[] = cadena.split("\\"+Character.toString(punto),3);
+		String resultado[] = cadena.split("\\"+Character.toString(punto),4);
 		System.out.println("tamaño resultado " + resultado.length);
 		for (int i = 0; i < resultado.length; i++) {
 			System.out.println("cadena  numero " + i + " " + resultado[i]);
